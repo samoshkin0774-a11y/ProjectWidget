28ce190b5efea48ccc64bc072f0df425754378bf
egor_samoshkin <iegorsamoshkin@yandex.ru>
2025-11-01 21:27:32 +0500

    recycling py file

--- src/processing.py
+++ src/processing.py
@@ -13,11 +13,5 @@ def sort_by_date(transactions: list[dict], reverse: bool = True) -> list[dict]:
     """
     Сортирует список словарей по дате с обработкой ошибок!
     """
-    try:
-        return sorted(transactions, key=lambda x: x['date'], reverse=reverse)
-    except KeyError:
-        print("Ошибка: в одном из словарей отсутствует ключ 'date'")
-        return transactions
-    except Exception as e:
-        print(f"Ошибка при сортировке: {e}")
-        return transactions
+    return sorted(transactions, key=lambda x: x['date'], reverse=reverse)
+
--- src/widget.py
+++ src/widget.py
@@ -1,27 +1,24 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+
 def mask_account_card(input_string: str) -> str:
     """
-    Маскируем номер карты или счета в зависимости от типа
+    Маскируем номер карты или счета в зависимости
 
     """
     # Разделяем строку на части
     parts = input_string.split()
 
-    # Определяем тип (первые слова до номера)
+    # Определяем тип
     account_type = " ".join(parts[:-1])
     number = parts[-1]
 
     # Проверяем, является ли это счетом.
-    if "счет" in account_type.lower() or "Счет" in account_type:
-        # Маскировка для счета
-        masked_number = f"**{number[-4:]}"
-        return f"{account_type} {masked_number}"
-    else:
-        # Маскировка для карты
-        if len(number) < 16:
-            return input_string  # Неверный формат номера карты
+    if "счет" in account_type.lower() :
 
-        masked_number = f"{number[:4]} {number[4:6]}** **** {number[-4:]}"
-        return f"{account_type} {masked_number}"
+        return f"счет {get_mask_account(number)}"
+    else:
+        return f"{account_type} {get_mask_card_number(number)}"
 
 
 def get_date(date_string: str) -> str:
@@ -31,11 +28,7 @@ def get_date(date_string: str) -> str:
     """
     from datetime import datetime
 
-    try:
-        # переносим дату из строки
-        dt = datetime.fromisoformat(date_string.replace("Z", "+00:00"))
+    dt = datetime.fromisoformat(date_string.replace("Z", "+00:00"))
 
-        # Форматируем в нужный формат
-        return dt.strftime("%d.%m.%Y")
-    except (ValueError, AttributeError):
-        return date_string  # Возвращаем исходную строку при ошибке
+    return dt.strftime("%d.%m.%Y")
+
